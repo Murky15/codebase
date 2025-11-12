0f6953081ec02fcc6c65166d1b409b256a3fde9c
Murky Spoon <bulletman27@icloud.com>
2025-11-11 21:48:36 -0500

    Cleanup, remove old/archived projects and components

--- code/dumb_level_editor/build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-tcc -Icode -I./code/dumb_level_editor/raylib/include ./code/dumb_level_editor/level_editor.c ./code/dumb_level_editor/raylib/lib/raylib.dll -o build/dumb_level_editor.exe
